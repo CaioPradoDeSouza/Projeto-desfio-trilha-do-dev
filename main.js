8b0881595efe0f2e40601ce63a0913d5abca357f
cpsso <cpssouza7@gmail.com>
2025-07-24 14:22:11 -0300

    feat: update css

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//const showButton = document.querySelectorAll(".buttonArr")
 
-document.querySelectorAll('.buttonArr').forEach(button => {
+
+document.querySelectorAll('.buttonArr ').forEach(button => {
     button.addEventListener('click', () => {
         const card = button.closest('.cardSixtSectionContent');
         const content = card.querySelector('.sixtSectionContentText');
@@ -12,6 +12,16 @@ document.querySelectorAll('.buttonArr').forEach(button => {
     });
 });
 
+document.querySelectorAll('.sixtSectionContentTitle').forEach(button => {
+    button.addEventListener('click', () => {
+        const card = button.closest('.cardSixtSectionContent');
+        const content = card.querySelector('.sixtSectionContentText');
 
+        content.classList.toggle('hidden');
+
+
+        card.classList.toggle('active');
+    });
+});
 
 
