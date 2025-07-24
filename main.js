005a4f274f1ed5d7d869d0cb2fbb4fff1fc434bd
cpsso <cpssouza7@gmail.com>
2025-07-23 21:04:29 -0300

    Feat: update style.css

--- main.js
+++ main.js
@@ -5,26 +5,13 @@ document.querySelectorAll('.buttonArr').forEach(button => {
         const card = button.closest('.cardSixtSectionContent');
         const content = card.querySelector('.sixtSectionContentText');
 
-        // Toggle visibilidade do texto
         content.classList.toggle('hidden');
 
-        // Toggle do fundo com classe 'active'
+
         card.classList.toggle('active');
     });
 });
 
-
-// function showContent(){
-//     let conten = document.getElementById("sixtSectionContentText")
-
-//     if(conten.classList.contains('hidden')){
-//         conten.classList.remove('hidden')
-//         conten.classList.add('showing')
-//     }else{
-//         conten.classList.remove('showing')
-//         conten.classList.add('hidden')
-//     }
-// }
 
 
 
